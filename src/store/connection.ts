d6e9433c8675cbce0213cd31c1a56dd48020f1f0
moonrailgun <moonrailgun@gmail.com>
2022-11-29 15:58:43 +0800

    perf: 优化连线逻辑，移除不可能出现的连线情况
    exec只能一对一
    port可以一对多但是不允许多对一

--- src/store/connection.ts
+++ src/store/connection.ts
@@ -3,7 +3,7 @@ import { TaichuNodePortType } from './node';
 import { persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 import { generateNodeId } from '../utils/string-helper';
-import { uniqBy } from 'lodash-es';
+import { uniqBy, without } from 'lodash-es';
 
 export interface ConnectInfo {
   id: string;
@@ -45,7 +45,7 @@ interface ConnectionState {
 export const useConnectionStore = create<ConnectionState>()(
   persist(
     immer((set, get) => ({
-      connections: [],
+      connections: [] as ConnectInfo[],
       workingConnection: null,
       startConnect: (
         fromNodeId,
@@ -68,60 +68,109 @@ export const useConnectionStore = create<ConnectionState>()(
         });
       },
       endConnect: (toNodeId, toNodePinName, toNodePinType, toNodeDirection) => {
-        const { workingConnection, cancelConnect, connections } = get();
+        const { workingConnection, cancelConnect } = get();
 
         if (!workingConnection) {
           return;
         }
 
+        const {
+          fromNodeId,
+          fromNodePinName,
+          fromNodePinType,
+          fromNodeDirection,
+        } = workingConnection;
+
         // 正在处于连接状态
-        if (toNodeId === workingConnection.fromNodeId) {
+        if (toNodeId === fromNodeId) {
           // 连接到相同节点
           cancelConnect();
           return;
         }
 
-        if (toNodeDirection === workingConnection.fromNodeDirection) {
+        if (toNodeDirection === fromNodeDirection) {
           // 相同方向
           cancelConnect();
           return;
         }
 
-        if (toNodePinType !== workingConnection.fromNodePinType) {
+        if (toNodePinType !== fromNodePinType) {
           // 不匹配(譬如exec和port连接)
           cancelConnect();
           return;
         }
 
-        set({
-          connections: uniqBy(
+        set((state) => {
+          // exec只能一对一
+          // port能够一对多(但是不能多对1)
+          console.log(fromNodePinType, fromNodeDirection);
+          if (
+            toNodePinType === 'exec' ||
+            (fromNodePinType === 'port' && fromNodeDirection === 'in-out')
+          ) {
+            const list = state.connections.filter(
+              (conn) =>
+                (conn.fromNodeId === fromNodeId &&
+                  conn.fromNodePinName === fromNodePinName) ||
+                (conn.toNodeId === fromNodeId &&
+                  conn.toNodePinName === fromNodePinName)
+            );
+
+            if (list.length > 0) {
+              state.connections = without(state.connections, ...list); // 移除不能多连的旧的连线
+            }
+          } else if (fromNodePinType === 'port') {
+            // 如果是port类型，则需要移除实际connection的to端口的其他连线
+            let list: ConnectInfo[] = [];
+            if (fromNodeDirection === 'out-in') {
+              list = state.connections.filter(
+                (conn) =>
+                  conn.toNodeId === toNodeId &&
+                  conn.toNodePinName === toNodePinName
+              );
+            } else if (fromNodeDirection === 'in-out') {
+              list = state.connections.filter(
+                (conn) =>
+                  conn.toNodeId === fromNodeId &&
+                  conn.toNodePinName === fromNodePinName
+              );
+            }
+
+            if (list.length > 0) {
+              state.connections = without(state.connections, ...list); // 移除不能多连的旧的连线
+            }
+          }
+
+          // 推入最新的连线
+          state.connections.push(
+            fromNodeDirection === 'out-in'
+              ? {
+                  id: generateNodeId(),
+                  fromNodeId: fromNodeId,
+                  fromNodePinName: fromNodePinName,
+                  toNodeId: toNodeId,
+                  toNodePinName: toNodePinName,
+                }
+              : {
+                  id: generateNodeId(),
+                  fromNodeId: toNodeId,
+                  fromNodePinName: toNodePinName,
+                  toNodeId: fromNodeId,
+                  toNodePinName: fromNodePinName,
+                }
+          );
+
+          // 连接线去重
+          state.connections = uniqBy(state.connections, (item) =>
             [
-              ...connections,
-              workingConnection.fromNodeDirection === 'out-in'
-                ? {
-                    id: generateNodeId(),
-                    fromNodeId: workingConnection.fromNodeId,
-                    fromNodePinName: workingConnection.fromNodePinName,
-                    toNodeId: toNodeId,
-                    toNodePinName: toNodePinName,
-                  }
-                : {
-                    id: generateNodeId(),
-                    fromNodeId: toNodeId,
-                    fromNodePinName: toNodePinName,
-                    toNodeId: workingConnection.fromNodeId,
-                    toNodePinName: workingConnection.fromNodePinName,
-                  },
-            ],
-            (item) =>
-              [
-                item.fromNodeId,
-                item.fromNodePinName,
-                item.toNodeId,
-                item.toNodePinName,
-              ].join('|')
-          ),
+              item.fromNodeId,
+              item.fromNodePinName,
+              item.toNodeId,
+              item.toNodePinName,
+            ].join('|')
+          );
         });
+
         cancelConnect();
       },
       cancelConnect: () => {
