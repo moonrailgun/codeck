2d7f03af1459d15d245543868fd5db1b29d37935
moonrailgun <moonrailgun@gmail.com>
2022-12-04 22:29:52 +0800

    refactor: move to packages and add tailwind purge

---
+++ packages/codeck/tailwind.config.cjs
@@ -0,0 +1,12 @@
+const path = require('path')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    content: [path.resolve(__dirname, './src/**/*.{js,jsx,ts,tsx}')],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
