5e704016224e3812e16256509af1eb812f7403ab
moonrailgun <moonrailgun@gmail.com>
2022-12-19 00:57:30 +0800

    feat: 内置依赖与调整在线运行模态框的大小与样式
    避免国内网络无法正常加载
    并移除的默认的背景颜色

--- platform/src/components/modal/RunCode.tsx
+++ platform/src/components/modal/RunCode.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Modal } from '@arco-design/web-react';
+import './RunCode.less';
 
 /**
  * 打开运行代码的模态框
@@ -8,6 +9,7 @@ import { Modal } from '@arco-design/web-react';
 export function openRunCodeModal(code: string) {
   Modal.confirm({
     title: 'Run Code Result',
+    className: 'run-code-modal',
     footer: null,
     icon: null,
     content: <RunCodeModal code={code} />,
@@ -32,16 +34,16 @@ export const RunCodeModal: React.FC<{ code: string }> = React.memo((props) => {
       `<!DOCTYPE html>
   <style>
     html, body{
-      background-color: black;
       color: white;
       margin: 0;
       padding: 0;
     }
   </style>
   <body>
+    <div>运行日志:</div>
     <ul id="console-log"></ul>
   </body>
-  <script type="application/javascript" src="//cdn.rawgit.com/Alorel/console-log-html/master/console-log-html.min.js"></script>
+  <script type="application/javascript" src="/lib/console-log-html.min.js"></script>
   <script>
     ConsoleLogHTML.connect(document.getElementById("console-log"));
   </script>
